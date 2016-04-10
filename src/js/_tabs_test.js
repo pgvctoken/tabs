d24e9983243069049d179a98c29497149bcd1740
Kenneth McMahon <kmcmahon@phoenix-group.com>
2016-04-10 23:27:35 +0000

    Finished first frontend test

--- src/js/_tabs_test.js
+++ src/js/_tabs_test.js
@@ -10,8 +10,33 @@
        });
         
         it("hides an element", function() {
-            tabs.initialize();
+            // arange
+            var element = addElement("div");
+            // act
+            tabs.initialize(element);
+        
+            // assert
+            assert.equal(getDisplayProperty(element), "none");
+            
+            //reset
+            removeElement(element);
         });
+        
+        function addElement(tagName) {
+            var element = document.createElement(tagName);
+            document.body.appendChild(element);
+            return element;
+        }
+        
+        function getDisplayProperty(element) {
+            var styles = getComputedStyle(element);
+            return styles.getPropertyValue("display");
+            
+        }
+        
+        function removeElement(element) {
+            element.parentNode.removeChild(element);            
+        }
         
     });
     
